bfe2c370abab04944a9d8a3df0b22de8441de4cc
MuriloAv <161392424+MuriloAv@users.noreply.github.com>
2025-06-10 12:47:16 -0300

    sOME cHANGES

--- Game.py
+++ Game.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from Const import WIN_WIDTH, WIN_HEIGHT
 from Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
         while True:
